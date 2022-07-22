639f0c5b43f5f90ee130bb55df6de7bfce949766
hong <hshong0907@gmail.com>
2022-07-22 21:30:41 +0900

    1st

---
+++ codingtest/src/codingtest/test10172.java
@@ -0,0 +1,14 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class test10172 {
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
